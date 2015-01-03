6dbd333112c80963ac39a2acecfdcc39f122181f
Vishal Chitravanshi <vishal_chitravanshi@yahoo.com>
2015-01-03 13:27:51 +0530

    Bug 124: Patient app: Do not allow login if patient is inactive.

--- App/main/src/main/java/com/doctl/patientcare/main/utility/Constants.java
+++ App/main/src/main/java/com/doctl/patientcare/main/utility/Constants.java
@@ -15,7 +15,7 @@ public final class Constants {
     public final static String VITAL_DETAIL_URL = SERVER_URL + "/api/treatment/" + API_VERSION + "/vitals/";
     public final static String PERSONAL_DETAIL_URL = SERVER_URL + "/api/account/" + API_VERSION + "/personal/";
     public final static String IMAGE_UPLOAD_URL = SERVER_URL + "/api/account/" + API_VERSION + "/image/";
-    public final static String LOGIN_URL = SERVER_URL + "/api-token-auth/";
+    public final static String LOGIN_URL = SERVER_URL + "/api/account/v1.0/login/";
     public final static String REGISTER_URL = SERVER_URL + "/api/account/v1.0/register/";
 
     public final static String PERSONAL_DETAIL_SHARED_PREFERENCE_NAME= "personal_detail";
